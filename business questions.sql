97690b6eb0b5a1635d137219e32abf9e6f5d3929
DomKup <domkp@aol.com>
2025-02-17 12:15:44 +0100

    business questions.sql

--- business questions.sql
+++ business questions.sql
@@ -1,5 +1,3 @@
-select count(*) from product_category_name_translation;
-
 -- What categories of tech products does Magist have?
 SELECT DISTINCT
     p.product_category_name
@@ -13,7 +11,8 @@ WHERE
         'informatica_acessorios',
         'pc_gamer', 'telefonia');
 
--- How many products of these tech categories have been sold (within the time window of the database snapshot)?  -- 16852
+-- How many products of these tech categories have been sold (within the time window of the database snapshot)?  
+-- ANSWER: 16852
 SELECT
     count(p.product_category_name) as tech_sales
 FROM
@@ -29,7 +28,8 @@ WHERE
         'pc_gamer', 'telefonia');
 
 
--- What’s the average price of the products being sold? -- 108,50€ for tech products, 120,65€ in total
+-- What’s the average price of the products being sold? 
+-- ANSWER: 108,50€ for tech products, 120,65€ in total
 SELECT AVG (price) FROM order_items;
 
 SELECT 
@@ -44,8 +44,8 @@ WHERE
         'informatica_acessorios',
         'pc_gamer', 'telefonia');
 
--- Are expensive tech products popular? -- 4148 products, 24,6% of total tech sales lie over the average price of 121€
-
+-- Are expensive tech products popular? 
+-- ANSWER: 4148 products, 24,6% of total tech sales lie over the average price of 121€
 SELECT
     count(product_category_name) as tech_sales
 FROM
@@ -60,7 +60,8 @@ WHERE
 	AND price >121.00;
 
 
--- How many months of data are included in magist database? -- 24 months
+-- How many months of data are included in magist database? 
+-- ANSWER: 24 months
 SELECT 
     TIMESTAMPDIFF(MONTH,
         MIN(earliest_date),
@@ -76,15 +77,15 @@ FROM
     FROM
         orders) AS combined_data;
 
-
--- How many sellers are there? 3095 
+-- How many sellers are there? 
+-- ANSWER: 3095 
 SELECT 
     COUNT(DISTINCT seller_id)
 FROM
     sellers;
 
--- How many Tech sellers are there? -- 477
-
+-- How many Tech sellers are there? 
+-- ANSWER: 477
 SELECT 
     COUNT(DISTINCT sellers.seller_id) AS tech_seller_count
 FROM
@@ -101,7 +102,8 @@ WHERE
         'pc_gamer',
         'telefonia');
         
--- What percentage of overall sellers are Tech sellers? -- 15,4%
+-- What percentage of overall sellers are Tech sellers? 
+-- ANSWER: 15,4%
 SELECT 
     (COUNT(DISTINCT CASE
             WHEN
@@ -122,15 +124,15 @@ FROM
         JOIN
     products ON order_items.product_id = products.product_id;
 
--- What is the total amount earned by all sellers? 2.321.963,9€ of 16.008.872€
-
+-- What is the total amount earned by all sellers? 
+-- ANSWER: 16.008.872€
 SELECT 
     SUM(payment_value)
 FROM
     order_payments;
 
--- What is the total amount earned by all Tech sellers? 2.321.963,9€
-
+-- What is the total amount earned by all Tech sellers? 
+-- ANSWER: 2.321.963,9€
 SELECT 
     SUM(payment_value) AS tech_earnings
 FROM
@@ -148,8 +150,8 @@ WHERE
         'pc_gamer', 'telefonia');
         
 
--- Can you work out the average monthly income of all sellers? 172,7€ 
-
+-- Can you work out the average monthly income of all sellers? 
+-- ANSWER: 172,7€ 
 SELECT 
     (SUM(payment_value) / COUNT(sellers.seller_id)) AS monthly_income
 FROM
@@ -159,8 +161,8 @@ FROM
         JOIN
     sellers ON order_items.seller_id = sellers.seller_id;
 
--- Can you work out the average monthly income of Tech sellers? 188€
-
+-- Can you work out the average monthly income of Tech sellers? 
+-- ANSWER: 188€
 SELECT 
     (SUM(payment_value) / COUNT(sellers.seller_id)) AS monthly_income
 FROM
@@ -178,15 +180,16 @@ WHERE
         'pc_gamer', 'telefonia');
         
 
---     What’s the average time between the order being placed and the product being delivered? -- 12,2 days
-
+-- What’s the average time between the order being placed and the product being delivered? 
+-- ANSWER: 12,2 days
 SELECT 
     AVG(TIME_TO_SEC(TIMEDIFF(order_delivered_customer_date,
                     order_purchase_timestamp)) / 86400) AS avg_delivery_days
 FROM
     orders;
     
--- Whats the average delivery time for tech products? -- 12,7 days
+-- Whats the average delivery time for tech products? 
+-- ANSWER: 12,7 days
 SELECT 
     AVG(TIME_TO_SEC(TIMEDIFF(order_delivered_customer_date,
                     order_purchase_timestamp)) / 86400) AS avg_delivery_days
@@ -205,8 +208,22 @@ WHERE
     AND order_purchase_timestamp IS NOT NULL;
 
 --     How many orders are delivered on time vs orders delivered with a delay? 
--- 6535 deliveries delayed by at least 24hrs, while 2754 were delivered on the estimated day and 90.152 were delivered earlier.
+-- ANSWER: 6535 deliveries delayed by at least 24hrs, while 92.906 were delivered on the estimated day or earlier.
+SELECT 
+    CASE
+        WHEN
+            (TIME_TO_SEC(TIMEDIFF(order_delivered_customer_date,
+                            order_estimated_delivery_date))) > 86400
+        THEN
+            'The deliveries were delayed'
+        ELSE 'The deliveries were on time'
+    END AS Timing,
+    COUNT(*) AS Amount
+FROM
+    orders
+GROUP BY Timing;
 
+-- Delays filtered for tech products only
 SELECT 
     CASE
         WHEN
@@ -232,36 +249,8 @@ WHERE
         'telefonia')
 GROUP BY Timing;
 
-SELECT 
-    CASE
-        WHEN
-            (TIME_TO_SEC(TIMEDIFF(order_delivered_customer_date,
-                            order_estimated_delivery_date))) > 86400
-        THEN
-            'The deliveries were delayed'
-        ELSE 'The deliveries were on time'
-    END AS Timing,
-    COUNT(*) AS Amount
-FROM
-    orders
-GROUP BY Timing;
-
---     Is there any pattern for delayed orders, e.g. big products being delayed more often? 
-
-SELECT 
-    *
-FROM
-    orders
-        JOIN
-    order_payments ON orders.order_id = order_payments.order_id
-        JOIN
-    order_items ON orders.order_id = order_items.order_id
-        JOIN
-    products ON order_items.product_id = products.product_id
-WHERE
-    (TIME_TO_SEC(TIMEDIFF(order_delivered_customer_date,
-                    order_estimated_delivery_date))) > 86400;
-
+-- Is there any pattern for delayed orders, e.g. big products being delayed more often? 
+-- a) standard code
 SELECT 
     *
 FROM
@@ -276,7 +265,7 @@ WHERE
     (TIME_TO_SEC(TIMEDIFF(order_delivered_customer_date,
                     order_estimated_delivery_date))) > 86400;
                     
--- Delayed deliveries of packages under 10kg
+-- b) Delayed deliveries of packages under 10kg
 SELECT 
     orders.order_id,
     product_weight_g,
@@ -298,7 +287,7 @@ WHERE
         AND product_weight_g <= 10000;
     
         
--- Sale trend of profits for tech products
+-- Selling trend of profits for tech products
 SELECT 
     order_purchase_timestamp,
     payment_value,
@@ -323,10 +312,7 @@ FROM
     FROM
         orders) AS combined_data;
 
-
-
 -- Sales of high end tech products: 1.820.974€ from 2.869.170€ in all tech
-
 SELECT 
     SUM(payment_value) AS tech_earnings
 FROM
